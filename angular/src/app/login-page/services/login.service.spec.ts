91b77b48fe7aab93939238a1b3a182a418fb688b
Miebach <moritz.miebach@capgemini.com>
2020-07-27 14:43:34 +0200

    updated owner-overview-page

--- angular/src/app/login-page/services/login.service.spec.ts
+++ angular/src/app/login-page/services/login.service.spec.ts
@@ -10,3 +10,15 @@ describe('LoginService', () => {
     expect(service).toBeTruthy();
   });
 });
+import { TestBed } from '@angular/core/testing';
+
+import { LoginService } from './login.service';
+
+describe('LoginService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: LoginService = TestBed.get(LoginService);
+    expect(service).toBeTruthy();
+  });
+});
